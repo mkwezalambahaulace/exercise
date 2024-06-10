e35d0387f9e403cc2a046946ed9f6fb1acc6b59a
mkwezalambahaulace <167177178+mkwezalambahaulace@users.noreply.github.com>
2024-06-09 20:38:42 -0700

    box1

---
+++ Box.h
@@ -0,0 +1,20 @@
+#pragma Once
+#include <iostream>
+using namespace std;
+class Box {
+    public :
+    Box();
+    Box(const double newLength, const double newBreadth, const double newheight);
+    ~Box ();
+    double GetVolume();
+    void SetLength(double len);
+    void SetBreadth(double bre);
+    void SetHeight(double hei);
+    Box operator+(const Box& b );
+    
+    private:
+    double length;
+    double breadth;
+    double height;
+};
+
